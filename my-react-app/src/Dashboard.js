1d2acd58313a810af9ae9d657a2447b26498e6f5
Your Name <you@example.com>
2024-10-16 13:38:53 +0300

    Updated homepage and dashboard design

--- my-react-app/src/Dashboard.js
+++ my-react-app/src/Dashboard.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import './App.css'; // Import your CSS file
+import './hhm.css'; // You can use this for external CSS
 
-const Dashboard = () => {
-    return (
-        <div className="background">
-            
-        <div>
-            <h1>Welcome to your Dashboard!</h1>
-            {/* Add more dashboard content here */}
+const App = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      <nav className="navbar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
+      </nav>
+
+      {/* Dashboard Section */}
+      <div className="dashboard">
+        <div className="dashboard-item">
+          <h3>Dashboard Item 1</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
         </div>
+        <div className="dashboard-item">
+          <h3>Dashboard Item 2</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
         </div>
-    );
+        <div className="dashboard-item">
+          <h3>Dashboard Item 3</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Dashboard;
+export default App;
